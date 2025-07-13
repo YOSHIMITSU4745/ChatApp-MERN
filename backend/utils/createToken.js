8589cee3d18ac4d195f513927bde942dcca0737f
Abhay jaggi <abhayjaggi2@gmail.com>
2025-07-13 06:12:50 +0530

    allowing cookie to travell cross sites due to seperate deploy

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,7 +7,7 @@ console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 res.cookie('jwt' ,token , {
     httpOnly:true,
     secure:process.env.NODE_ENV !=="development",
-    sameSite:'strict',
+    sameSite:'None',
     maxAge:30*24*60*60*1000
 } );
 
