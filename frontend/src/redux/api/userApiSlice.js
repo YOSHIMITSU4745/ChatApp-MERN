7abd5dc3f8a146419720a4e8fcd924bc3002cdb7
Abhay jaggi <abhayjaggi2@gmail.com>
2025-08-20 21:51:24 +0530

    credentials include and samesite in cookie to none

--- frontend/src/redux/api/userApiSlice.js
+++ frontend/src/redux/api/userApiSlice.js
@@ -12,7 +12,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
                 url:`${USERS_URL}/auth`,
                 method:'POST',
                 body:data,
-                credentials:true,
+                credentials:"include",
             }),
         }),
 
